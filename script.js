f2f2c498b74f24f81a510108b1d91259f530f9c9
noawennemars1 <55925884+noawennemars1@users.noreply.github.com>
2020-12-09 15:57:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var datum = new Date ("Jan 15, 2021").getTime(); //hier maak ik een var aan voor de datum dat de timer moet aflopen
+var tijd = setInterval(function () { //om elke seconden de tijd up te daten
+    var nu = new Date().getTime();
+    var verschil = datum - nu; //het verschil tussen de datum en nu is de tijd die nog resterend is, dus die wordt hier berekend
+    var dagen = Math.floor(verschil / (1000 * 60 * 60 * 24));//tijd berekenen voor de dagen
+    var uren = Math.floor((verschil % (1000 * 60 * 60) / (1000 * 60)));//tijd berekenen voor de uren
+    var minuten = Math.floor(verschil % (1000 * 60 * 60) / (1000 * 60));//tijd berekenen voor de minuten
+    var seconden = Math.floor(verschil % (1000 * 60) / 1000);//tijd berekenen voor de seconden
+
+    document.getElementById("countdown").innerHTML = dagen + "d " + uren + "u " + minuten + "m " + seconden + "s ";
+    if (verschil < 0){
+        clearInterval(tijd);
+        document.getElementById("countdown").innerHTML = "afgelopen";
+    }
+})

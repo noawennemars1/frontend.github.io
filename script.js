a3f859d940299605041a303e748142b567409eab
noawennemars1 <55925884+noawennemars1@users.noreply.github.com>
2021-01-14 14:34:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -18,12 +18,11 @@ var datum = new Date ("Jan 16, 2021").getTime(); //hier maak ik een var aan voor
 var tijd = setInterval(function () { //om elke seconden de tijd up te daten
     var nu = new Date().getTime();
     var verschil = datum - nu; //het verschil tussen de datum en nu is de tijd die nog resterend is, dus die wordt hier berekend
-    var dagen = Math.floor(verschil / (1000 * 60 * 60 * 24));//tijd berekenen voor de dagen
     var uren = Math.floor((verschil % (1000 * 60 * 60) / (1000 * 60)));//tijd berekenen voor de uren
     var minuten = Math.floor(verschil % (1000 * 60 * 60) / (1000 * 60));//tijd berekenen voor de minuten
     var seconden = Math.floor(verschil % (1000 * 60) / 1000);//tijd berekenen voor de seconden
 
-    document.getElementById("countdown").innerHTML = dagen + "d " + uren + "u " + minuten + "m " + seconden + "s ";
+    document.getElementById("countdown").innerHTML =  + uren + "u " + minuten + "m " + seconden + "s ";
     if (verschil < 0){
         clearInterval(tijd);
         document.getElementById("countdown").innerHTML = "afgelopen";

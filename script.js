53e3768fd22a8e35a1fc1b997ce6b1e3f03041dc
noawennemars1 <55925884+noawennemars1@users.noreply.github.com>
2021-01-15 14:29:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,25 +4,29 @@
 
 var menuIcoon = document.querySelector('#menuIcoon');
 var nav = document.querySelector('#menu');
-var body = document.querySelector('body')
+var body = document.querySelector('body');
+var menuSluiten = document.querySelector('#sluitIcoon');
+var nav = document.querySelector('#menu');
+var datum = new Date ("Jan 16, 2021").getTime(); //hier maak ik een var aan voor de datum dat de timer moet aflopen
+var tijd = setInterval(function () { //om elke seconden de tijd up te daten
+var nu = new Date().getTime();
+var verschil = datum - nu; //het verschil tussen de datum en nu is de tijd die nog resterend is, dus die wordt hier berekend
+var uren = Math.floor((verschil % (1000 * 60 * 60) / (1000 * 60)));//tijd berekenen voor de uren
+var minuten = Math.floor(verschil % (1000 * 60 * 60) / (1000 * 60));//tijd berekenen voor de minuten
+var seconden = Math.floor(verschil % (1000 * 60) / 1000);//tijd berekenen voor de seconden
 
 function openMenu() {
     nav.classList.toggle('toonnav');
     body.classList.toggle('nietscrollen');
 }
+function sluitMenu() {
+    nav.classList.toggle('navsluiten');
+}
 
 menuIcoon.addEventListener("click", openMenu);
+sluitIcoon.addEventListener("click", sluitMenu);
 
-
-var datum = new Date ("Jan 16, 2021").getTime(); //hier maak ik een var aan voor de datum dat de timer moet aflopen
-var tijd = setInterval(function () { //om elke seconden de tijd up te daten
-    var nu = new Date().getTime();
-    var verschil = datum - nu; //het verschil tussen de datum en nu is de tijd die nog resterend is, dus die wordt hier berekend
-    var uren = Math.floor((verschil % (1000 * 60 * 60) / (1000 * 60)));//tijd berekenen voor de uren
-    var minuten = Math.floor(verschil % (1000 * 60 * 60) / (1000 * 60));//tijd berekenen voor de minuten
-    var seconden = Math.floor(verschil % (1000 * 60) / 1000);//tijd berekenen voor de seconden
-
-    document.getElementById("countdown").innerHTML =  + uren + "u " + minuten + "m " + seconden + "s ";
+document.getElementById("countdown").innerHTML =  + uren + "u " + minuten + "m " + seconden + "s ";
     if (verschil < 0){
         clearInterval(tijd);
         document.getElementById("countdown").innerHTML = "afgelopen";
